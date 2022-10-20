ff09660707c1b1385cca54a1b1c3e55031a4460c
shakhbozpro <shakhbozmamatkulov111@gmail.com>
2022-10-20 10:30:34 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Slick carousel codes
+// $('.slider-list').slick({
+//     centerMode: true,
+//     slidesToShow: 3,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     variableWidth: true,
+//     infinite: true,
+//     responsive: [
+//         {
+//             breakpoint: 1000,
+//             settings: {
+//                 centerMode: true,
+//                 slidesToShow: 2
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 centerMode: true,
+//                 slidesToShow: 1
+//             }
+//         }
+//     ]
+// });
